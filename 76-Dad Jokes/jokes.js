8be41ec2cb64ac02a1d054ed168432e344398676
Nhi Ngo <nhi.nxyn@gmail.com>
2020-02-27 22:29:27 -0500

    76 - Dad Jokes

---
+++ 76-Dad Jokes/jokes.js
@@ -0,0 +1,51 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  // turn loader on
+  loader.classList.remove('hidden');
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  // turn loader off
+  loader.classList.add('hidden');
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log('Ahhh we used that one last time');
+    console.log(not);
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleButtonClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButtonSpan.textContent = randomItemFromArray(
+    buttonText,
+    jokeButtonSpan.textContent
+  );
+  // console.log(jokeButtonSpan.textContent);
+}
+
+jokeButton.addEventListener('click', handleButtonClick);
