411b2e4203d658aeeb0b58c3937f7a15f4c4389c
nhi-ngo <nhingo.y@gmail.com>
2020-03-30 18:38:57 -0400

    34 - Click Outside Modal

---
+++ 34-Click Outside Modal/click-outside.js
@@ -0,0 +1,47 @@
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+
+  // Grab the image src and description
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+
+  // Populate the modal with the new info
+  modalInner.innerHTML = `
+    <img width="600" height ="600" src="${imgSrc.replace(
+      '200',
+      '600'
+    )}" alt="${name}"/>
+      <p>${desc}</p>
+  `;
+
+  // Show the modal
+  modalOuter.classList.add('open');
+}
+
+cardButtons.forEach(btn =>
+  btn.addEventListener('click', handleCardButtonClick)
+);
+
+// Close the modal
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
